dd702887909e480bbcb46567b9cd36d9fa0fdf6a
Trong Nguyen <nvtrong@tma.com.vn>
2018-10-03 17:49:08 +0700

    Them trang thai user connect

--- index.js
+++ index.js
@@ -4,15 +4,40 @@ var app = express();
 var server = require('http').Server(app);
 var port = (process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 6969);
 var io = require('socket.io')(server);
+var userOnile = []
+var userAwait = []
 
 server.listen(port, () => console.log('Server running in port ' + port));
 
 io.on('connection', function(socket){
+	userOnile.push(socket.id)
   console.log(socket.id + ': connected');
+  console.log(userOnile);
   socket.emit('id', socket.id);
 
   socket.on('disconnect', function(){
+	userOnile.remove(socket.id);
     console.log(socket.id + ': disconnected')
+	var idSocket = socket.id+"a"
+	io.sockets.emit(idSocket, true)
+	console.log(idSocket)
+  })
+
+  socket.on('ketnoi', data =>{
+	  var user1 = data.user
+	  userAwait.remove(user1)
+	  userAwait.push(user1)
+	  console.log(userAwait)
+		var user2 = getRandomUser(user1, userAwait)
+		if(user2 == null){
+			console.log("abc null ")
+		}else{
+			var room = user2
+			io.sockets.emit(user1, {myId: user1, guestId: user2, room:room})
+			io.sockets.emit(user2, {myId: user2, guestId: user1, room:room})
+			console.log(room)
+		}
+	  
   })
 
   socket.on('newMessage', data => {
@@ -26,3 +51,29 @@ io.on('connection', function(socket){
 app.get('/', (req, res) => {
     res.send("Home page. Server running okay.");
 })
+
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this
+}
+
+getRandomUser = (user, items) =>{
+	var item = items[Math.floor(Math.random()*items.length)];
+	if(items.length <= 1){
+		return null
+	}
+	
+	if(item==user){
+		return getRandomUser(user, items)
+	}else{
+		items.remove(item)
+		items.remove(user)
+		return item
+	}
+}
